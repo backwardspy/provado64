c4836647d3757394146151a81ee2527679140bc1
Chris Latham <backwardspy@gmail.com>
2019-01-18 20:47:42 +0000

    sprite rendering

--- c64test.s
+++ c64test.s
@@ -4,71 +4,60 @@
 + .word 0 ; end of BASIC line
 
 * = $1000
-
 start
-  ;; set mode 0 (ecm/bmm/mcm 0)
+  jsr m0
+  jsr m0_cls
+
+  ;; set ptr & enable sprite 0
+  lda #spr_smile / 64
+  sta $07f8
+  sta $d000
+  sta $d001
+  lda #$01
+  sta $d015
+
+-
+  jmp -
+
+  rts
+
+;;; select vic mode 0 (standard character mode)
+;;; input:
+;;; output:
+;;; uses: A
+m0
   lda $d011
   and #%10011111
   sta $d011
   lda $d016
   and #%11101111
   sta $d016
-
-  ;; clear the screen
-  lda #$20
-  ldx #0
-cls_loop
-  sta $0400, x
-  inx
-  bne cls_loop
-
-loop
-  ;; set background & border colours
-  lda col
-  sta $d020
-
-  lda col+1
-  sta $d021
-
-  ;; set text colour
-  lda col+2
-  sta $0286
-
-  ;; print msg
-  ldx #0
-p_loop
-  lda msg, x
-  beq done
-  sta $05c0,x
-  lda col+2
-  sta $d9c0,x
-  inx
-  jmp p_loop
-done
-  inc col
-  inc col+1
-  inc col+2
-
-  ;; wait for jiffy clock low byte/high nibble to change
-  lda $00a2
-  and #$f0
-  sta time
-wait
-  lda $00a2
-  and #$f0
-  cmp time
-  beq wait
-
-  jmp loop
   rts
 
-msg
-  .enc screen
-  .text "hello from 6502 asm", 0
-  .enc none
+;;; clear the screen. only valid in mode 0.
+;;; input:
+;;; output:
+;;; uses: A, X
+m0_cls
+  lda #$20
+  ldx #0
+-
+  sta $0400, x
+  sta $0500, x
+  sta $0600, x
+  sta $0700, x
+  inx
+  bne -
+  rts
 
-col
-  .byte $00, $01, $02           ; border, background, text
-
-time
-  .byte $00
+;;; sprites
+* = $2000
+spr_smile
+  .byte $00,$ff,$00,$07,$ff,$e0,$0f,$00
+  .byte $f0,$18,$00,$18,$30,$00,$0c,$60
+  .byte $00,$06,$61,$83,$06,$c1,$c3,$83
+  .byte $c1,$c3,$83,$c0,$00,$03,$c0,$00
+  .byte $03,$c0,$00,$03,$c0,$ff,$03,$c0
+  .byte $ff,$03,$60,$7e,$06,$60,$3c,$06
+  .byte $30,$00,$0c,$18,$00,$18,$0f,$00
+  .byte $f0,$07,$ff,$e0,$00,$ff,$00,$01
