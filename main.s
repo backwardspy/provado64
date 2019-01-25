e3fc11081d13274da4f45307721318ffbce43505
Chris Latham <backwardspy@gmail.com>
2019-01-25 23:55:43 +0000

    multicolour sprites

--- main.s
+++ main.s
@@ -1,41 +1,54 @@
 * = $0801
 
-  .byte $0C,$08,$0A,$00,$9E,$20,$38,$31,$39,$32,$00,$00,$00
+.byte $0C,$08,$0A,$00,$9E,$20,$33,$32,$37,$36,$38,$00,$00,$00
 
 * = $1000
 
 .binary "music.sid", $7e
 
-* = $2000
+* = $8000
 
 sid_init = $1000
-sid_play = $1006
+sid_play = $1003
 
-entry           sei             ; disable interrupts
+entry           lda $0001       ; switch out basic rom to free up $a000
+                and #%11111110
+                sta $0001
+
+                lda $dd00       ; select vic bank 2
+                and #%11111100
+                ora #%00000001
+                sta $dd00
 
                 jsr initscr     ; blackout
-                jsr putmsg      ; write text
+
+                jsr initspr     ; initialise sprites
+
+                lda #$00
+                tax
+                tay
                 jsr sid_init    ; start music
 
-                ldy #$7f        ; turn off CIA interrupts
-                sty $dc0d
-                sty $dd0d
-                lda $dc0d       ; consume exciting CIA interrupts
-                lda $dd0d
+                sei
 
-                lda #$01
-                sta $d01a       ; request interrupt for raster beam
+                lda #$7f        ; disable cia/vic interrupts
+                sta $dc0d
+                sta $dd0d
 
-                lda #<irq       ; point irq to custom handler
+                lda #$01        ; enable raster interrupts
+                sta $d01a
+
+                lda #<irq       ; hijack interrupt routine
                 ldx #>irq
                 sta $0314
                 stx $0315
 
-                lda #$00
-                sta $d012       ; trigger irq on first line
-                lda $d011       ; ensure 9th bit is zero
-                and #$7f
-                sta $d011
+                ldy #0          ; interrupt at line 0
+                sty $d012
+
+                lda $dc0d       ; clear existing interrupts
+                lda $dd0d
+                asl $d019
 
                 cli             ; re-enable interrupts
                 jmp *
@@ -45,10 +58,10 @@ initscr         ldx #0
                 stx $d021
 
 clear           lda #$20        ; space char
-                sta $0400, x    ; screen ram
-                sta $0500, x
-                sta $0600, x
-                sta $06e8, x
+                sta $8400, x    ; screen ram
+                sta $8500, x
+                sta $8600, x
+                sta $86e8, x
                 lda #$0e
                 sta $d800, x
                 sta $d900, x
@@ -58,48 +71,43 @@ clear           lda #$20        ; space char
                 bne clear       ; loop until overflow
                 rts
 
-.enc "screen"
-line1           .text "      what if that doesn't happen       "
-line2           .text " because our security is amazingly good "
-.enc "none"
+initspr         lda #50         ; set sprite 0 pos
+                sta $d000
+                sta $d001
 
-putmsg          ldx #0
-_loop           lda line1, x
-                sta $0590, x
-                lda line2, x
-                sta $05e0, x
+                lda #0          ; set x coord 9th bits to 0
+                sta $d010
+
+                lda #$0b        ; sprite multicolour 1
+                sta $d025
+                lda #$0a        ; sprite multicolour 2
+                sta $d026
+
+                lda #$ff        ; set all sprites to MCM
+                sta $d01c
+                sta $d017       ; ... and make them L A R G E
+                sta $d01d
+
+                lda #(pigeon_neutral - $8000) / 64
+                sta $87f8
+
+                lda #1          ; enable sprite 0
+                sta $d015
+
+                rts
+
+irq             jsr sid_play    ; continue music
+
+                ldx $87f8
                 inx
-                cpx #40         ; 40 columns
-                bne _loop
-                rts
+                cpx #(pigeon_flap - $8000) / 64
+                bne _noreset
+                ldx #(pigeon_neutral - $8000) / 64
+_noreset        stx $87f8
 
-colwash         ldx #39         ; iterate columns 39-0
-                lda pal+39      ; start on last colour
-_cycle1         ldy pal-1, x
-                sta pal-1, x
-                sta $d990, x    ; store to colour ram
-                tya             ; transfer original colour to a
-                dex
-                bne _cycle1     ; repeat until zero
-                sta pal+39      ; store last colour at end
-                sta $d990       ; ^ and colour ram
-
-                ldy #0          ; iterate columns 0-39
-                ldx #39         ; while iterating palette 39-0
-_cycle2         lda pal, x
-                sta $d9e0, y
-                iny
-                dex
-                bne _cycle2
-                rts
-
-irq             dec $d019       ; ack irq
-                jsr colwash     ; update palette
-                jsr sid_play    ; continue music
+                dec $d019       ; ack irq
                 jmp $ea81       ; hand off to kernal irq
 
-pal             .byte $0d, $0d, $0f, $0f, $05, $05, $0c, $0c
-                .byte $08, $08, $0b, $0b, $09, $09, $09, $09
-                .byte $09, $09, $09, $09, $09, $09, $09, $09
-                .byte $09, $09, $09, $09, $0b, $0b, $08, $08
-                .byte $0c, $0c, $05, $05, $0f, $0f, $0d, $0d
+* = $a000
+
+.include "pigeon.s"
